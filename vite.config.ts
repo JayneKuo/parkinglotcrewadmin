7257b1c23ed04112c91cf2fc196752375a79d7e5
zhenyan.guo <zhenyan.guo@item.com>
2024-12-11 18:17:01 +0800

    解决部署失败

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
-      'vue': 'vue/dist/vue.esm-bundler.js'
+      '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true
   }
 })
