2acea04cf962411e209241a5b0a6da6b09897864
zhenyan.guo <zhenyan.guo@item.com>
2024-12-11 18:22:08 +0800

    解决部署失败

--- vite.config.ts
+++ vite.config.ts
@@ -12,22 +12,12 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        }
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       }
     }
-  },
-  server: {
-    port: 3000,
-    host: true,
-    open: true
   }
 })
