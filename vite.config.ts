91b7ecd82891a6c8ee485948eafa879cbbae0a83
zhenyan.guo <zhenyan.guo@item.com>
2024-12-12 10:34:07 +0800

    部署

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false
+      }
+    }
+  },
   build: {
     outDir: 'dist',
     minify: 'esbuild',
